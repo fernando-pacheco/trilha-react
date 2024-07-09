650a2085c416b856be01725ccb57219694d562b6
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-07-09 14:45:25 -0300

    [feat] main frame

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,16 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6 text-center">
+      <p className="text-zinc-300 text-lg">Convide seus amigos e planeje a sua próxima viagem!</p>
+      <div>
+
+      </div>
+      <p>
+        Ao planejar sua viagem pela plann.er você automaticamente concorda
+        com nossos <a href="#" className="">termos de uso</a> e <a href="#" className="">políticas de privacidade</a>.
+      </p>
+    </div>
+    </div>
   )
 }
